d17166c0e02cf18afe46cb184dde6940294a0922
mzakioddin <mzakioddin@entrata.com>
2024-05-21 14:37:52 +0530

    JS changes

--- src/test.js
+++ src/test.js
@@ -1,1 +1,23 @@
-console.log("HI Zaki ");
+// program to check if the number is even or odd
+// take input from the user
+const number = 55;
+
+//check if the number is even
+if (number % 2 == 0) {
+  console.log("The number is even.");
+}
+
+// if the number is odd
+else {
+  console.log("The number is odd.");
+}
+
+// program to check if the number is even or odd
+// take input from the user
+const number1 = 88;
+
+// ternary operator
+const result = number1 % 2 == 0 ? "even" : "odd";
+
+// display the result
+console.log(`The number is ${result}.`);
