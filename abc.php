dda7169ed71f714d3ad434f2ec8955187ced3695
Muhammed Zakioddin <87798787+m-zakioddin@users.noreply.github.com>
2024-05-21 10:55:52 +0530

    Update abc.php

--- abc.php
+++ abc.php
@@ -16,5 +16,16 @@ for( $i=0; $i >=0; $i++ ) {
     echo 'Hi';
 }
 
+for( $i=0; $i >=0; $i++ ) {
+    echo 'Hi';
+}
+
+for( $j=0; $j >=0; $j++ ) {
+    echo 'Hi';
+}
+
+for( $j=0; $j >=0; $j++ ) {
+    echo 'Hi';
+}
 
 ?>
